b4d639fd5bc5f31b89034680bd1565c1ca4afc2e
Mario <mariocamaraneto@gmail.com>
2017-04-24 22:28:10 -0300

    Configura painel de administração
    Irá possuir opções de Usuários, Grupos, Empregados e Departamentos.

--- employee/admin.py
+++ employee/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Employee, Department
+
+admin.site.site_header = 'Employee Manager'
+
 
 # Register your models here.
+admin.site.register(Employee)
+admin.site.register(Department)
