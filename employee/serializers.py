3d13625569614c04a65144f118a309b77bb8b5b0
Mario <mariocamaraneto@gmail.com>
2017-04-27 16:48:33 -0300

    Serialização e deserialização personalizada
    Como a entidade Employee possui a relação com Department, a serialização
    não é gerada automatizamente, sendo assim, foi sobreescrita a função de
    create e de update.
    Ao receber uma criação ou atualização é verificado se o departamento já
    existe no banco de dados:
    -se sim, busca entendidade Department e vincula a nova entidade Employee
    -se não, cria-se uma nova entidade Department com o nome enviado e
    vincula com no novo Employee
    
    *A entidade Department so poderá ser criada por meio de requisições de
    criação e atualizações de Employee. Retira-se assim a complixidade de
    manipulação de department por porte da API. Contudo, no painel de
    administação é possível altera-los. (Requisito criado para demonstração
    de habildades)

--- employee/serializers.py
+++ employee/serializers.py
@@ -2,15 +2,38 @@ from .models import Department, Employee
 from rest_framework import serializers
 
 
-class DepartmentSerializer(serializers.HyperlinkedModelSerializer):
+class DepartmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Department
         fields = ('name',)
 
 
-class EmployeeSerializer(serializers.HyperlinkedModelSerializer):
+class EmployeeSerializer(serializers.ModelSerializer):
     department = serializers.CharField(source='department.name')
     
     class Meta:
         model = Employee
         fields = ('name', 'email', 'department', )
+        
+    def create(self, validated_data):
+        """
+        Cria e retorna uma nova instacia de Employee
+        """
+        department, _ = Department.objects.get_or_create(name=validated_data['department']['name'])
+        validated_data['department'] = department
+        
+        return Employee.objects.create(**validated_data)
+    
+    def update(self, instance, validated_data):
+        """
+        Atualiza e retorna uma nova instacia de Employee
+        """
+        instance.name = validated_data.get('name', instance.name)
+        instance.email = validated_data.get('email', instance.email)
+        
+        departmentName = validated_data.get('department', {}).get('name', instance.department.name)
+        instance.department, _ = Department.objects.get_or_create(name=departmentName)
+        
+        instance.save()
+        return instance
+    
--- employee/views.py
+++ employee/views.py
@@ -1,13 +1,30 @@
 from .models import Department, Employee
 from rest_framework import viewsets
+from rest_framework import mixins
+from rest_framework import generics
 from .serializers import DepartmentSerializer, EmployeeSerializer
 
 # Create your views here.
 
 class EmployeeViewSet(viewsets.ModelViewSet):
+    """
+    Classe que retorna requisições REST da entidade Employee
+    Operações aceitas: listagem, recuperação de registro, criação, atualização e remoção.
+    Ref: http://www.django-rest-framework.org/api-guide/viewsets/#modelviewset
+    """
     queryset = Employee.objects.all().order_by('-created_at')
     serializer_class = EmployeeSerializer
 
-class DepartmentViewSet(viewsets.ModelViewSet):
+class DepartmentViewSet(mixins.ListModelMixin,
+                        mixins.RetrieveModelMixin,
+                        mixins.UpdateModelMixin,
+                        viewsets.GenericViewSet):
+    """
+    Classe que retorna requisições REST da entidade Department
+    Operações aceitas: listagem e recuperação de um registro.
+        Função de criação desabilitado julgando que todos departamentos serão
+        criados dinâmicamente conforme adiciona-se novos Employees.
+    Ref: http://www.django-rest-framework.org/api-guide/viewsets/#custom-viewset-base-classes
+    """    
     queryset = Department.objects.all().order_by('name')
     serializer_class = DepartmentSerializer
